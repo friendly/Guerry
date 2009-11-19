56610095b106702cc5a5cecb862740b0734c3be2
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-19 22:57:18 +0000

    Added thinnedSpatialPoly; included MultiSpat.Rnw
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/guerry/pkg@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/thinnedSpatialPoly.R
@@ -0,0 +1,34 @@
+thinnedSpatialPoly <- function(SP, tolerance, minarea=0) {
+  if (!require(shapefiles)) stop("shapefiles package is required")
+  stopifnot(inherits(SP, "SpatialPolygons"))
+
+	# TODO: determine and set defaults for tolerance, minarea  
+	# TODO: suppress warnings: "In Polygon(crds_s) : Non-finite label point detected and replaced"
+  pls <- slot(SP, "polygons")
+  pls_dp <- vector(mode="list", length=length(pls))
+  for (i in 1:length(pls)) {
+    Pls <- slot(pls[[i]], "Polygons")
+    Pls_dp <- vector(mode="list", length=length(Pls))
+    for (j in 1:length(Pls)) {
+      crds <- slot(Pls[[j]], "coords")
+      crds_s <- dp(list(x=crds[,1], y=crds[,2]), tolerance=tolerance)
+      crds_s <- do.call("cbind", crds_s)
+      if(!identical(crds_s[1,], crds_s[nrow(crds_s),]))
+        crds_s <- rbind(crds_s, crds_s[1,])
+      Pls_dp[[j]] <- Polygon(crds_s)
+    }
+    Keep <- logical(length(Pls_dp))
+    for (j in 1:length(Pls_dp)) {
+      Keep[j] <- TRUE
+      if (slot(Pls_dp[[j]], "area") < minarea) Keep[j] <- FALSE
+    }
+    Pls_dp <- Pls_dp[Keep]
+    pls_dp[[i]] <- Polygons(Pls_dp, ID=slot(pls[[i]], "ID"))
+  }
+  SP_dp <- SpatialPolygons(pls_dp, proj4string=slot(SP, "proj4string"))
+  if(inherits(SP, "SpatialPolygonsDataFrame")) {
+    data <- slot(SP, "data")
+    SP_dp <- SpatialPolygonsDataFrame(SP_dp, data=data)
+  }
+  SP_dp
+}
