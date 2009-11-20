ddf607668fa391743453ee86ee8196e0031e8000
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-20 14:13:02 +0000

    Updated thinnedSpatialPoly
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/guerry/pkg@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/thinnedSpatialPoly.R
+++ R/thinnedSpatialPoly.R
@@ -2,8 +2,13 @@ thinnedSpatialPoly <- function(SP, tolerance, minarea=0) {
   if (!require(shapefiles)) stop("shapefiles package is required")
   stopifnot(inherits(SP, "SpatialPolygons"))
 
-	# TODO: determine and set defaults for tolerance, minarea  
+	# set default for tolerance unless supplied
+	if (missing(tolerance))  {
+		bbox <- slot(SP, "bbox")
+		tolerance <- min(bbox[,2]-bbox[,1])/100
+	}
 	# TODO: suppress warnings: "In Polygon(crds_s) : Non-finite label point detected and replaced"
+	ow <- options(warn=-1)
   pls <- slot(SP, "polygons")
   pls_dp <- vector(mode="list", length=length(pls))
   for (i in 1:length(pls)) {
@@ -25,7 +30,8 @@ thinnedSpatialPoly <- function(SP, tolerance, minarea=0) {
     Pls_dp <- Pls_dp[Keep]
     pls_dp[[i]] <- Polygons(Pls_dp, ID=slot(pls[[i]], "ID"))
   }
-  SP_dp <- SpatialPolygons(pls_dp, proj4string=slot(SP, "proj4string"))
+  options(ow)
+	SP_dp <- SpatialPolygons(pls_dp, proj4string=slot(SP, "proj4string"))
   if(inherits(SP, "SpatialPolygonsDataFrame")) {
     data <- slot(SP, "data")
     SP_dp <- SpatialPolygonsDataFrame(SP_dp, data=data)
