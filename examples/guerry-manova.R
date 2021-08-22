1f43e7c360dd0fc139f648b0c9d566ae459f30e9
Michael Friendly <friendly@yorku.ca>
2021-08-22 14:59:35 -0400

    guerry-manova

--- examples/guerry-manova.R
+++ examples/guerry-manova.R
@@ -21,35 +21,38 @@ col.region   <- colors()[c(149, 254, 468, 552, 26)] # colors for region
 
 #df %>% mutate_all(funs(dense_rank(desc(.))))
 
-df %>% multate(
-               Crime_pers = dense_rank(Crime_pers),
-               Crime_prop = dense_rank(Crime_prop),
-               Literacy   = dense_rank(Literacy),
-               Donations  = dense_rank(Donations),
-               Infants    = dense_rank(Infants),
-               Suicides   = dense_rank(uicides)
+df %>% mutate(
+             Crime_pers = dense_rank(Crime_pers),
+             Crime_prop = dense_rank(Crime_prop),
+             Literacy   = dense_rank(Literacy),
+             Donations  = dense_rank(Donations),
+             Infants    = dense_rank(Infants),
+             Suicides   = dense_rank(Suicides)
 ) -> df_ranks
 
 
 
 # What variables predict crime?
-crime.mod <- lm(cbind(Crime_pers, Crime_prop) ~ Region + Literacy + Donations +  Infants + Suicides, data=Guerry)
+crime.mod <- lm(cbind(Crime_pers, Crime_prop) ~ 
+                  Region + Literacy + Donations +  Infants + Suicides, data=Guerry)
 Anova(crime.mod)
 
 heplot(crime.mod, fill=TRUE, fill.alpha=0.1)
 
-
-
-
+crime.can <- candisc(crime.mod)
+crime.can
+plot(crime.can)
 
 # What variables discriminate among Regions ?
 
-region.mod <- lm(cbind(Crime_pers, Crime_prop, Literacy, Donations, Infants, Suicides) ~ Region, data=Guerry)
+region.mod <- lm(cbind(Crime_pers, Crime_prop, Literacy, Donations, Infants, Suicides) ~ 
+                   Region, data=Guerry)
+Anova(region.mod)
 
 heplot(region.mod, fill=TRUE, fill.alpha=0.1)
 
 pairs(region.mod, variables=1:3, fill=TRUE, fill.alpha=0.1)
 
-guerry.can <- candisc(region.mod)
-guerry.can
-plot(guerry.can)
+region.can <- candisc(region.mod)
+region.can
+plot(region.can)
