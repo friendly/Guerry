9338a386b5fcc8496086b280cb6a54e9a0f87d63
Michael Friendly <friendly@yorku.ca>
2021-08-22 05:37:42 -0400

    work on guerry-manova

--- examples/guerry-manova.R
+++ examples/guerry-manova.R
@@ -5,7 +5,32 @@
 library(heplots)
 library(candisc)
 library(car)
+library(Guerry)
+library(dplyr)
 data(Guerry, package="Guerry")
+
+# extract pieces
+df           <- data.frame(Guerry)[, 4:9]    # the 6 variables
+france.map   <- as(gfrance85, "SpatialPolygons") # the map
+xy           <- coordinates(gfrance)           # spatial coordinates
+dep.names    <- data.frame(Guerry)[, 3]       # departement names
+region.names <- data.frame(Guerry)[, 2]       # region names
+col.region   <- colors()[c(149, 254, 468, 552, 26)] # colors for region
+
+# ranks
+
+#df %>% mutate_all(funs(dense_rank(desc(.))))
+
+df %>% multate(
+               Crime_pers = dense_rank(Crime_pers),
+               Crime_prop = dense_rank(Crime_prop),
+               Literacy   = dense_rank(Literacy),
+               Donations  = dense_rank(Donations),
+               Infants    = dense_rank(Infants),
+               Suicides   = dense_rank(uicides)
+) -> df_ranks
+
+
 
 # What variables predict crime?
 crime.mod <- lm(cbind(Crime_pers, Crime_prop) ~ Region + Literacy + Donations +  Infants + Suicides, data=Guerry)
