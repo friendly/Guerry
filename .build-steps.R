576100824cc14f583f8831d6beae7dd0a8a2cb0a
Michael Friendly <friendly@yorku.ca>
2022-10-07 16:19:21 -0400

    make vignettes more comparable

--- .build-steps.R
+++ .build-steps.R
@@ -1,16 +1,17 @@
 
-# deal with compression
+# deal with compression -- DONE
 paths <- sort(Sys.glob(c("data/gf*.RData")))
 tools::checkRdaFiles(paths)
-
 tools::resaveRdaFiles(paths)
 
 # for qpdf
-Sys.setenv(R_GSCMD="C:/Program Files/gs/gs9.53.3/bin/gswin64c.exe")
+Sys.setenv(R_GSCMD="C:/Program Files/gs/gs9.53.3/bin/gswin64c.exe")  # home
+Sys.setenv(R_GSCMD="C:/Program Files/gs/gs9.56.0/bin/gswin64c.exe")  # work
+# qpdf need to be on the PATH
+Sys.which(Sys.getenv("R_QPDF", "qpdf"))
 
 
-
-args = c('--compact-vignettes=both')
+args = c('--compact-vignettes=both')  # don't really need this anymore
 devtools::check(args=args)
 
 # no longer need --resave-data
